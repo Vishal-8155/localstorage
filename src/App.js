ebda7d868ed5c5f20c5e96ecd47e9a08b6d82ead
Vishal-8155 <121939382+Vishal-8155@users.noreply.github.com>
2024-01-07 11:29:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Localstoragecomponent from './components/Localstoragecomponent';
+import Sessionstoragecomponent from './components/Sessionstoragecomponent';
+
+function App() {
+  return (
+    <div>
+      <Localstoragecomponent/>
+      {/* <Sessionstoragecomponent/> */}
+    </div>
+  );
+}
+
+export default App;
